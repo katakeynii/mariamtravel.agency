794ead544d32d9da4d73dbeb6d880a8f60559de2
Mohamed Camara GUEYE <mohamed-camara.gueye@free-partenaires.sn>
2025-01-13 16:12:09 +0000

    updating deploy

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -1,7 +1,11 @@
 import mixpanel from "mixpanel-browser";
+import { env } from "process";
  
 // Near entry of your product, init Mixpanel
-mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_APP_KEY as string, {
+console.log("mixpanel key", process.env.NEXT_PUBLIC_MIXPANEL_APP_KEY)
+const MIX_KEY = process.env.NEXT_PUBLIC_MIXPANEL_APP_KEY ?? "";
+console.log("mixpanel key", MIX_KEY)
+mixpanel.init(MIX_KEY, {
   debug: true,
   track_pageview: true,
   persistence: "localStorage",
